6f1d7c6a5874bed261310c55750d8a0dbbc706cb
Serena Geroe <serenasurfer@gmail.com>
2023-09-27 20:35:42 -0400

    Final version. Added comments, small changes in code

--- digital_palette_client.py
+++ digital_palette_client.py
@@ -1,44 +1,61 @@
 #!/usr/bin/env python3
-# client
+# client program
 #
-# Prompts user for an instruction, sends instruction to the server
+# Serena Geroe
+#
+# Prompts user for an instruction, sends instruction to the server, and receives the server's reply
 
 import socket
 
 HOST = "127.0.0.1"  # This is the loopback address
 PORT = 65432        # The port used by the server
 
-def client_driver():
-    print("Initiating connection to server. IP", HOST, ", Port", PORT)
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        print(f"Connection established")
-        while True:
-            # loop until the user asks to quit
-            if not talk_to_server(s):
+def client_connect_w_server(): # function that drives the client
+
+    '''
+    This function creates a socket object, connects with the server using this socket,
+    opens an infinite loop to keep the connection open so the server continues to
+    listen for a message from the client, and calls the talk_to_server() function.
+    '''
+
+    print("\nInitiating connection to server. IP", HOST, ", Port", PORT,"\n")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: # creates the socket object for a specified address and port number
+        s.connect((HOST, PORT)) # connects to the server
+        print(f"Connection established\n")
+        while True: # loop until the user asks to quit
+            if not talk_to_server(s): # calls the function to communicate messages with the server
                 break
 
 def talk_to_server(sock):
+    '''
+    This function manages the interfaces between the client and the server, and the (human) user and the client.
+    It specifies how to format the instruction input and formats the server's reply.
+    Then, it sends the user's message to the server, listens and waits for the server's reply, receives the 
+    server's reply, and communicates it with the human user.
+    '''
+    # welcome message
     msg = input("Welcome to the Digital Palette!\n" +
                 "What two different primary colors would you like to blend, or from which two primary colors should I randomly choose?\n" +
                 "Please enter your input in the following format (excluding the '<>' characters): " + 
                 "<blend or choose>,<primary color 1>,<primary color 2> \n"+
-                "Enter quit to exit this program.\n")
-    if msg == 'quit':
-        print("Client quitting per operator request\nExiting the Digital Palette")
+                "Enter quit to exit this program.\n\n")
+    if msg == 'quit': # Check whether user seeks to exit the program
+        print("\nClient quitting per operator request\n") # Communicate to the user the program is ending
         return False
-    print(f"Sending message '{msg}' to server")
-    sock.sendall(msg.encode('utf-8'))
-    print("Message sent, awaiting reply")
-    reply = sock.recv(4096)
+    print(f"\nSending message '{msg}' to server\n") # print message that is going to be sent to the server
+    sock.sendall(msg.encode('utf-8')) # this line (1) encodes the message to be sent to the server to be type byte, and (2)
+                                      # sends the message to the server
+    print("Message sent, awaiting reply\n")
+    reply = sock.recv(1024) # receives the message from the server
 
-    if not reply:
+    if not reply: # tells the client to keep listening for a reply from the server
         return False
     else:
-        print(f"Received server's reply: '{reply}'")
+        print(f"Received server's reply: '{reply}'. Thank you.\n") # communicate the server's reply to the user
         return reply
 
+
+# run the code! First, run client_connect_w_server(). Then, notify the user the program is ending.
 if __name__ == "__main__":
-    client_driver()
-print("End of program\n" +
-      "Exiting the Digital Palette")
+    client_connect_w_server()
+print("End of program. Exiting the Digital Palette\n")
--- digital_palette_server.py
+++ digital_palette_server.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # server program
 #
+# Serena Geroe
+#
 # Listens for and receives a message from the client. Returns a response to the client's instruction/inquiry.
 
 import socket
@@ -21,13 +23,15 @@ def digital_palette(instruction):
     '''
     primary_colors = ["red","yellow","blue"] # define a list of primary colors; these are the only possible valid inputs for nouns one and two
     instruction_list = instruction.split(",") #parse the client's instructions based on a comma delimeter
-    print("instruction list:" ,  instruction_list, "length: ", len(instruction_list)) # verify in terminal client instruction
+    print("instruction list:" ,  instruction_list, ", length: ", len(instruction_list), "\n") # verify in terminal client instruction
     
     if len(instruction_list) == 3: # first, set these values to assist with error handling -- but only if there are three arguments in
                                     # the client's instruction
         verb = instruction_list[0] # identify the verb of the instruction
         noun_one = instruction_list[1] # identify the first color from the instruction
         noun_two = instruction_list[2] # identify the second color from the instruction
+
+        print("verb is", verb, ", color 1 is", noun_one, ", color 2 is" , noun_two, "\n") # verify in terminal client instruction
 
         # invalid verb
         if (verb != "blend") and (verb != "choose"):
@@ -44,13 +48,11 @@ def digital_palette(instruction):
             
         # digital palette computations to generate server output
         if verb == "blend":
-            if "red" in instruction and "yellow" in instruction:
+            if (noun_one == "red" or noun_one == "yellow") and (noun_two == "red" or noun_two == "yellow"):
                 server_reply = "orange"
-            elif "yellow" in instruction and "blue" in instruction:
+            elif (noun_one == "yellow" or noun_one == "blue") and(noun_two == "yellow" or noun_two == "blue"):
                 server_reply = "green"
-            elif "blue" in instruction and "red" in instruction:
-                server_reply = "violet"
-            else: server_reply = "Please only enter primary colors to blend. These are: red, yellow, and blue. \n"
+            else: server_reply = "violet"
         else: # choose, by process of elimination (sinse the verb is either blend or choose, and it is not blend)
             rand_num_decider = random.randint(1,10) * random.randint(2,11)
             # print("Random number decider:" , str(rand_num_decider))
@@ -62,7 +64,7 @@ def digital_palette(instruction):
 
     # too few arguments, likely due to incorrect delimeter
     elif len(instruction_list) < 3: 
-        server_reply = "Invalid input: too few arguments -- ensure use of comma delimeter. Please enter three arguments separated by commas (1 verb and two colors)."
+        server_reply = "Invalid input: too few arguments. Ensure use of comma delimeter. Please enter three arguments separated by commas (1 verb and two colors)."
         return server_reply
     # too many arguments
     else:
@@ -70,23 +72,26 @@ def digital_palette(instruction):
         return server_reply
     
 
-print("Initiating connection to client - listening for connections at IP", HOST, "and port", PORT, " \n")
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: #creates socket object with address family AF_INET and socket type SOCK_STREAM
-    s.bind((HOST, PORT)) # associates the socket with the particular desired network interface and port number
-    s.listen() # enables the server to accept connections; also accounts for the server's backlogged connections (ones that haven't yet been accepted)
-    conn, addr = s.accept() # accept() blocks execution and waits for an incoming connection
-    with conn: # once a connection is made with the client, a new socket object is returned from accept() (different socket from the listening socket)
-        print(f"Established connection, {addr}")
-        while True: # infinite while loop to loop over blocking colls to conn.recv()
-            data = conn.recv(1024)
-            data_string = data.decode('utf-8')
-            # print(data_string)
-            if not data:
-                break # if conn.recv() returns an empty bytes object, the server knows the client closed the connection
-            print(f"Received client message: '{data!r}' [{len(data)} bytes]") # print client message
-            reply = digital_palette(data_string) # set the server's reply equal to the output of the digital_palette function
-            print("Digital Palette program output:" , reply)
-            reply_byte = reply.encode('utf-8') # encode the digital_palette's reply to be type byte instead of a string
-            conn.sendall(reply_byte) # send server response to the client
-           
-print("Server job complete!\n")
+# run the code!
+if __name__ == "__main__":
+    print("\nInitiating connection to client - listening for connections at IP", HOST, "and port", PORT, " \n")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: #creates socket object with address family AF_INET and socket type SOCK_STREAM
+        s.bind((HOST, PORT)) # associates the socket with the particular desired network interface and port number
+        s.listen() # enables the server to accept connections; also accounts for the server's backlogged connections (ones that haven't yet been accepted)
+        conn, addr = s.accept() # accept() blocks execution and waits for an incoming connection
+        with conn: # once a connection is made with the client, a new socket object is returned from accept() (different socket from the listening socket)
+            print(f"Established connection, {addr}\n")
+            while True: # infinite while loop to loop over blocking calls to conn.recv()
+                data = conn.recv(1024)
+                data_string = data.decode('utf-8')
+                # print(data_string)
+                if not data:
+                    break # if conn.recv() returns an empty bytes object, the server knows the client closed the connection
+                print(f"Received client message: '{data!r}' [{len(data)} bytes] \n") # print client message
+                reply = digital_palette(data_string) # set the server's reply equal to the output of the digital_palette function
+                print("Digital Palette program output:" , reply, "\n")
+                reply_byte = reply.encode('utf-8') # encode the digital_palette's reply to be type byte instead of a string
+                print("Sending output '", reply, "' back to client \n")
+                conn.sendall(reply_byte) # send server response to the client
+            
+    print("Server job complete!\n")
